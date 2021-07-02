e876fb96dcecaebb76706a7b2c34f1acef959b8c
19dstern <19dstern@student.dasd.org>
2021-07-02 15:12:50 -0400

    Fixed Add Items

--- app/src/main/java/com/example/grocerylist/GroceryItem.kt
+++ app/src/main/java/com/example/grocerylist/GroceryItem.kt
@@ -1,15 +1,27 @@
 package com.example.grocerylist
 
-import android.widget.ImageView
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-    @Entity(tableName = "grocery_table")
-    data class GroceryItem(//@PrimaryKey @ColumnInfo(name ="calories") var calories: Int,
-//                         @ColumnInfo(name ="food_image") var food_image: ImageView,
-//                         @ColumnInfo(name ="food_amount") var food_amount: Int,
-//                         @ColumnInfo(name ="price") var price: Double,
-                         @ColumnInfo (name = "food_name") var food_name:String
-//                         @ColumnInfo(name ="poster_path") var poster_path: String,)
-    )
+//    @Entity(tableName = "grocery_table")
+//    data class GroceryItem(//@PrimaryKey @ColumnInfo(name ="calories") var calories: Int,
+////                         @ColumnInfo(name ="food_image") var food_image: ImageView,
+////                         @ColumnInfo(name ="food_amount") var food_amount: Int,
+////                         @ColumnInfo(name ="price") var price: Double,
+//                         @ColumnInfo (name = "food_name") var food_name:String
+////                         @ColumnInfo(name ="poster_path") var poster_path: String,)
+//    )
+
+class GroceryItem (var foodName:String) {
+
+    fun setFood(food:String)
+    {
+        this.foodName = food
+    }
+
+    fun getFood():String {
+        return this.foodName
+    }
+
+}
