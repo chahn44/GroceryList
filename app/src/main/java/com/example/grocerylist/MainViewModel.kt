c874900ece714042995b2727adffac5962add9e2
Charlotte Hahn <chahn44@vt.edu>
2021-07-02 20:48:59 -0400

    firebase start

--- app/src/main/java/com/example/grocerylist/MainViewModel.kt
+++ app/src/main/java/com/example/grocerylist/MainViewModel.kt
@@ -1,28 +1,49 @@
 package com.example.grocerylist
 
-import android.content.ContentValues.TAG
-import android.util.Log
-import com.example.grocerylist.GroceryItem
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
+
 
 class MainViewModel : ViewModel() {
+
+    //fetch reference database
+    var db: FirebaseFirestore? = FirebaseFirestore.getInstance()
+    val listItems = db?.collection("listItems")?.document("groceries")
+
+    val source = Source.CACHE
 
     private var groceryList = ArrayList<GroceryItem>()//change back to grocery item
 
     fun addToList(item: GroceryItem)
     {
+        //update groceryList
         groceryList.add(item)
+
+        //re-send groceryList to database
+        //listItems.set()
+
     }
 
     fun getGroceryList(): ArrayList<GroceryItem> {
+        //set groceryList to list in database -> how to turn document into list?
+        //groceryList = listItems.get()
+
         return groceryList
     }
 
-    fun removeFromList(position: Int) {
+    fun removeFromList(name: GroceryItem) {
 
-        Log.d("Project", "model: remove from list; position = $position")
+        //update groceryList (done in list by listener)
+
+        //re-send groceryList to database
+
+        //Log.d("Project", "model: remove from list; position = $position")
         //groceryList.removeAt(position-1)
-    }
+//        db?.collection("listItems")?.document("$name")
+//            .delete()
+//            .addOnSuccessListener { Log.d("project", "DocumentSnapshot successfully deleted!") }
+//            .addOnFailureListener { e -> Log.w("project", "Error deleting document", e) }
 
+    }
 }
